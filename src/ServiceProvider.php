80c609a820fbe17a199ab53715b8d0056f0cecd7
hugh.li <hugh.li@foxmail.com>
2023-12-17 16:43:23 +0800

    feat: 函数计算不需要HttpsGuard

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -14,13 +14,10 @@ use HughCube\Laravel\AliFC\Actions\PreFreezeAction;
 use HughCube\Laravel\AliFC\Actions\PreStopAction;
 use HughCube\Laravel\AliFC\Commands\JobPayloadCommand;
 use HughCube\Laravel\AliFC\Queue\Connector;
-use HughCube\Laravel\Knight\Http\Middleware\HttpsGuard;
 use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
 use Illuminate\Queue\QueueManager;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
-use Illuminate\Support\Str;
 use Laravel\Lumen\Application as LumenApplication;
 
 /**
@@ -51,7 +48,6 @@ class ServiceProvider extends IlluminateServiceProvider
     {
         $this->bootCommands();
         $this->bootHandlers();
-        $this->bootHttpsGuard();
     }
 
     protected function bootCommands()
@@ -87,33 +83,5 @@ class ServiceProvider extends IlluminateServiceProvider
         if (false !== ($handler = config('alifc.handlers.pre_freeze', PreStopAction::class))) {
             Route::any('/pre-stop', $handler)->name('alifc_handler_pre_stop');
         }
-    }
-
-    protected function bootHttpsGuard()
-    {
-        if (! class_exists(HttpsGuard::class)) {
-            return;
-        }
-
-        HttpsGuard::customExcept(sprintf('%s-%s', md5(__METHOD__), crc32(__METHOD__)), function (Request $request) {
-            $fcHeaderCount = 0;
-            foreach ($request->headers->all() as $name => $values) {
-                if (Str::startsWith($name, 'x-fc-')) {
-                    $fcHeaderCount++;
-                }
-            }
-
-            if ($fcHeaderCount < 5) {
-                return false;
-            }
-
-            foreach (['initialize', 'invoke', 'pre-freeze', 'pre-stop'] as $uri) {
-                if ($request->fullUrlIs($uri) || $request->is($uri)) {
-                    return true;
-                }
-            }
-
-            return false;
-        });
     }
 }
