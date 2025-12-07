11ba36700297b63ba821ee530232cf44afc18287
hugh.li <hugh.li@foxmail.com>
2025-12-07 23:45:36 +0800

    PreStopAction 添加执行时长日志记录

--- src/Actions/PreStopAction.php
+++ src/Actions/PreStopAction.php
@@ -8,6 +8,25 @@
 
 namespace HughCube\Laravel\AliFC\Actions;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+
 class PreStopAction extends Action
 {
+    protected function action(): JsonResponse
+    {
+        $start = microtime(true);
+
+        $response = parent::action();
+
+        $end = microtime(true);
+        $duration = ($end - $start) * 1000;
+
+        Log::channel()->info(sprintf(
+            'PreStop action completed, duration: %.5fms',
+            $duration
+        ));
+
+        return $response;
+    }
 }
